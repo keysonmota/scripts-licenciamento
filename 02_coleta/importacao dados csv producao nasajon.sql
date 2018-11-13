e5da866511b31b572c03284d0a1e826d2c41b0e9
Keyson Mota <keysonmota@nasajon.com.br>
2018-11-13 12:25:13 -0200

    Adução de scripts utilizados para atualizar base de testes (local), baseados em importação de csv extraídos a partir da base de produção no redash.

---
+++ 02_coleta/importacao dados csv producao nasajon.sql
@@ -0,0 +1,37 @@
+﻿/*
+Ultima importação 11/10
+*/
+
+do
+$$
+begin
+
+raise notice 'limpa os dados';
+truncate itenscontratos,contratos, servicos;
+
+raise notice 'servicos';
+copy servicos from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\servicos.servicos.csv' with csv  HEADER delimiter '~';
+copy servicos from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\servicos.servicos2.csv' with csv  HEADER delimiter '~';
+update servicos set id_grupodeservico = '1d1f3055-8682-4de1-8d48-fa93091ab9ba' where upper(descricao) like '%SQL%';
+raise notice 'contratos';
+copy contratos from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\financas.contratos.csv' with csv  HEADER delimiter '~';
+raise notice 'itenscontratos';
+copy itenscontratos from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\financas.itenscontratos.csv' with csv  HEADER delimiter '~';
+
+
+--apaga contratos nao sql
+with marcar as (
+select distinct contrato from itenscontratos ic
+join servicos s on (s.id = ic.servico) 
+where id_grupodeservico  = '1d1f3055-8682-4de1-8d48-fa93091ab9ba' and not cancelado and recorrente
+)
+update contratos set participante = '00000000-0000-0000-0000-000000000000'::uuid where not contrato in (select contrato from marcar);
+end;
+$$;
+
+select * from itenscontratos where contrato = (
+select * from contratos where codigo = '2008-10856' and participante <>  '00000000-0000-0000-0000-000000000000'
+)
+--SELECT * FROM  DIRETORIO.instalacoes LIMIT 1
+
+select 
---
+++ 02_coleta/importacao dados csv producao.sql
@@ -0,0 +1,85 @@
+﻿/*
+Ultima importação 11/10
+*/
+
+do
+$$
+begin
+
+raise notice 'limpa os dados';
+truncate 
+DIRETORIO.licencasmetadados,diretorio.modulos,diretorio.atualizacoes,diretorio.historicos_validacoes,
+diretorio.instalacoes,diretorio.licencas_servidores,diretorio.licencas,diretorio.provisoes ,
+diretorio.tenants_administradores,diretorio.tenants_contas,diretorio.tenants_sistemas,diretorio.tenants_contas,
+diretorio.grupos_permissoes,diretorio.grupos_entidades,diretorio.grupos,
+diretorio.nasajon_sync,diretorio.tenants,diretorio.clientes,diretorio.servidores,
+diretorio.aplicacoes;
+
+truncate query_124;
+truncate query_125;
+
+
+raise notice 'apaga colunas novas';
+begin
+alter table diretorio.clientes drop cliente_erp_id;
+alter table diretorio.licencas drop contrato_id;
+exception
+when others then
+end;
+
+
+raise notice 'reinsere os dados';
+raise notice 'clientes';
+copy diretorio.clientes from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\clientes.csv' with csv  HEADER delimiter ',';
+raise notice 'consultas';
+insert into query_124 select codigo,razaosocial,nomefantasia,regexp_replace(cpf, '[^0-9]', '', 'g') cpf,regexp_replace(cnpj, '[^0-9]', '', 'g') cnpj,id from diretorio.clientes;
+copy query_125 from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\Licenciamento_- Clientes Nasajon_2018_10_31.csv' with csv  HEADER delimiter ',';
+update query_125 set chavecnpj = coalesce(cnpj,cpf) where not cnpj isnull or not cpf isnull;
+raise notice 'servidores';
+alter table diretorio.servidores alter column metadados type text;
+copy diretorio.servidores from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\servidores.csv' with csv  HEADER delimiter ',';
+raise notice 'aplicações';
+copy diretorio.aplicacoes from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\aplicacoes.csv' with csv  HEADER delimiter ',';
+raise notice 'licencas';
+truncate tmp_licencas;
+copy tmp_licencas from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\licencas.csv' with csv  HEADER delimiter ',';
+delete from diretorio.modulos;
+delete from diretorio.atualizacoes;
+delete from diretorio.instalacoes;
+delete from diretorio.licencas;
+insert into diretorio.licencas select * from tmp_licencas where cliente_id in (
+select id from diretorio.clientes
+);
+raise notice 'modulos';
+truncate tmp_modulos;
+copy tmp_modulos from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\modulos.csv' with csv  HEADER delimiter ',';
+insert into diretorio.modulos select * from tmp_modulos where licenca_id in (
+select id from diretorio.licencas
+);
+
+raise notice 'instalações';
+truncate tmp_instalacoes;
+copy tmp_instalacoes(id,cliente_id,versao,datacriacao,nome,licenca_id,hash,ativa,servidor_id,ipinstalacao,ipultimaverificacao,dataultimaverificacao,ultimostatus,ultimamensagem) from 'C:\SPRINTS\36\dados diretorio\tarefas diretorio\02_coleta\instalacoes.csv' with csv  HEADER delimiter ',';
+insert into diretorio.instalacoes select * from tmp_instalacoes where cliente_id in (
+  select id from diretorio.clientes
+)
+
+--copy diretorio.historicos_validacoes from 'C:\SPRINTS\36\dados diretorio\historicos_validacoes.csv' with csv  HEADER delimiter ',';
+
+update diretorio.clientes set cpf = cnpj where cpf is null and cnpj is not null;
+update diretorio.clientes set cnpj = cpf where cnpj is null and cpf is not null;
+
+alter table diretorio.clientes add cliente_erp_id uuid;
+COMMENT ON COLUMN diretorio.clientes.cliente_erp_id IS 'Guid do participante na base da Nasajon';
+
+alter table diretorio.licencas add contrato_id uuid;
+COMMENT ON COLUMN diretorio.licencas.contrato_id IS 'Guid do contrato na base da Nasajon';
+
+
+end;
+$$;
+
+
+--SELECT * FROM  DIRETORIO.instalacoes LIMIT 1
+
+
